d79167bca4b3bd016c03eb3e97490f02be7e0325
Matheus <kamaitatsu@gmail.com>
2022-08-05 22:55:06 -0300

    correçao

--- desafio5.sql
+++ desafio5.sql
@@ -1,9 +1,9 @@
 SELECT 
 M.musica_nome AS cancao,
-COUNT(H.musica_id_H) AS quantidade
+COUNT(H.musica_id_H) AS reproducoes
 FROM SpotifyClone.Musica M
 JOIN SpotifyClone.Historico H 
 ON M.id_musica = H.musica_id_H
 GROUP BY M.musica_nome
-HAVING quantidade = 2
+HAVING reproducoes = 2
 LIMIT 2
--- desafio6.sql
+++ desafio6.sql
@@ -1,6 +1,6 @@
 SELECT MIN(P.valor) AS "faturamento_minimo",
 MAX(P.valor) AS "faturamento_maximo",
-ROUND(AVG(P.valor), 2) AS "faturamento_total",
+ROUND(AVG(P.valor), 2) AS "faturamento_medio",
 SUM(P.valor) AS "faturamento_total"
 FROM SpotifyClone.Plano P
 JOIN SpotifyClone.Usuario U
