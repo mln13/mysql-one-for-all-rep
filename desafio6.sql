e923ce760cfcfe0d3c57a1b9527e220d1eafaed0
Matheus <kamaitatsu@gmail.com>
2022-08-05 21:57:08 -0300

    5, 6

---
+++ desafio6.sql
@@ -0,0 +1,7 @@
+SELECT MIN(P.valor) AS "faturamento_minimo",
+MAX(P.valor) AS "faturamento_maximo",
+ROUND(AVG(P.valor), 2) AS "faturamento_total",
+SUM(P.valor) AS "faturamento_total"
+FROM SpotifyClone.Plano P
+JOIN SpotifyClone.Usuario U
+ON U.plano_id_U = P.plano_id
